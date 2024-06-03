d51f35811f6bc640e734e76f87a6bf4e0d0f7711
zubayer-salehin <zubaersalehin@gmail.com>
2024-06-03 18:19:28 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
 
   return (
-    <div className="mt-80 text-xl font-semibold text-center">
+    <div className="mt-80 text-xl font-semibold text-center text-red-600">
       Google Anlaytics Check
     </div>
   );
