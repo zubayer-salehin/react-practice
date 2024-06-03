fb4bc2aee2d6b1d32256b4ab479ead3f3e0d9099
zubayer-salehin <zubaersalehin@gmail.com>
2024-06-03 18:25:54 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { useEffect } from 'react';
+import ReactGA from 'react-ga';
 
 function App() {
+
+  useEffect(() => {
+    ReactGA.initialize('G-BB1SVWXMMM');
+    ReactGA.pageview('/app');
+  }, [])
 
   return (
     <div className="mt-80 text-xl font-semibold text-center text-red-600">
